baae9fbcf8319149adaef5cb178d292658298d1e
V.Bonk <eltarya@gmail.com>
2023-05-06 21:59:21 +0300

    simple express project
    express, node.js, mongoDB

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import router from "./router.js";
+import fileUpload from 'express-fileupload';
+
+const PORT = 5000;
+const DB_URL = `mongodbUrl`
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
